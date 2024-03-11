fd3d1d8d33472d128054739e966c223c1d0a9e81
irameez <155886868+irameez705@users.noreply.github.com>
2024-03-12 03:10:20 +0500

    Add files via upload
    Done with comparison operators.

---
+++ index.ts
@@ -0,0 +1,13 @@
+//Comparision Operators
+
+let a = 6;
+let b = 3;
+let isEqual = (a == b); // false
+let isNotEqual = (a != b); // true
+let isGreaterThan = (a > b); // true
+let isLessThan = (a < b); // false
+
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
